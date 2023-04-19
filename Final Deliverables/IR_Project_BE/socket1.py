bde225a3928cf8e9e1b3abfa1f7d5b2c4d1132c9
bsumit43-iiitd <sumit22302@iiitd.ac.in>
2023-04-19 15:51:29 +0530

    Modification on BE

--- Final Deliverables/IR_Project_BE/socket1.py
+++ Final Deliverables/IR_Project_BE/socket1.py
@@ -5,4 +5,5 @@ def start_consumer():
     consumer = KafkaConsumer('locations', bootstrap_servers=['localhost:9092'])
 
     for msg in consumer:
+        print('HHHHHH')
         socketio.emit('message', {'data': msg.value.decode('utf-8')})
--- Final Deliverables/MapCreator/app.py
+++ Final Deliverables/MapCreator/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
-from readcsv import readcsv
 import signal
+import json
+
 from kafka import KafkaConsumer
 from threading import Thread
-from readData import generateData
-from batch import batch, exit_gracefully
 from flask_socketio import SocketIO, emit
 from map import createMap
-# from batch import Consumer
 
 app =   Flask(__name__)
 app.config['SECRET_KEY'] = 'ir'
@@ -16,7 +14,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
-    return generateData()
+    return 'Hello'
 
 @socketio.on('connect')
 def handle_connect():
@@ -29,15 +27,26 @@ def handle_connect():
         return False
     emit('message', 'Hello client!')
 
-
+# loc_arr=[]
 def start_consumer():
-    print('Locations Extracted')
-    # consumer = KafkaConsumer('locations', bootstrap_servers=['localhost:9092'])
-    # for msg in consumer:
-    #     html = createMap(msg.value.decode('utf-8'))
-    #     print(html)
-    #     print(msg.value.decode('utf-8'))
-    #     socketio.emit('locations',  msg.value.decode('utf-8'))
+    consumer = KafkaConsumer('locations', bootstrap_servers=['localhost:9092'])
+    for msg in consumer:
+        # loc_arr.extend(json.loads(msg.value.decode('utf-8')))
+        # print(json.loads(msg.value.decode('utf-8')))
+        # for i in json.loads(msg.value.decode('utf-8')):
+        #     print(i)
+        print(json.loads(msg.value.decode('utf-8')))
+        try:
+            html = createMap(json.loads(msg.value.decode('utf-8')))
+            # html = createMap(loc_arr)
+            print(html)
+            # print(msg.value.decode('utf-8'))
+            socketio.emit('locations',  html)
+        except Exception as e:
+            print(e)
+            print("An exception occurred")
+   
+
 
 @socketio.on('disconnect')
 def test_disconnect():
@@ -48,13 +57,6 @@ def test_disconnect():
     print('Client disconnected')
 
 if __name__ =="__main__":
-    # signal.signal(signal.SIGINT, exit_gracefully)
-    # signal.signal(signal.SIGTERM, exit_gracefully)
-    # start_consumer()
-    # app.run(host='127.0.0.1',port=8000, debug=True)
-    # socketio.run(app,port=8000, debug=True)
     socketio.start_background_task(start_consumer)
-    socketio.run(app, port=5000)
-    consumer_thread = Thread(target=batch)
-    consumer_thread.start()
+    socketio.run(app, port=8001)
     

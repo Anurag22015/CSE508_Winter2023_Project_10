48e3f9372898eb1329c245fae14ff85093356e0d
bsumit43-iiitd <122974730+bsumit43-iiitd@users.noreply.github.com>
2023-04-20 23:54:53 +0530

    Threshold creation (#6)

--- Final Deliverables/MapCreator/app.py
+++ Final Deliverables/MapCreator/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 import signal
 import json
-
+import threading
+import time
 from kafka import KafkaConsumer
 from threading import Thread
 from flask_socketio import SocketIO, emit
 from map import createMap
-
+loc_arr=[]
+interval = 30 
 app =   Flask(__name__)
 app.config['SECRET_KEY'] = 'ir'
 socketio = SocketIO(app, cors_allowed_origins='*')
@@ -27,7 +29,7 @@ def handle_connect():
         return False
     emit('message', 'Hello client!')
 
-# loc_arr=[]
+
 def start_consumer():
     consumer = KafkaConsumer('locations', bootstrap_servers=['localhost:9092'])
     for msg in consumer:
@@ -35,16 +37,30 @@ def start_consumer():
         # print(json.loads(msg.value.decode('utf-8')))
         # for i in json.loads(msg.value.decode('utf-8')):
         #     print(i)
-        print(json.loads(msg.value.decode('utf-8')))
-        try:
-            html = createMap(json.loads(msg.value.decode('utf-8')))
+        # print(json.loads(msg.value.decode('utf-8')))
+        loc_arr.extend(json.loads(msg.value.decode('utf-8')))
+
+def callMap():
+    global loc_arr
+    loc=[]
+    loc=loc_arr
+    loc_arr=[]
+    try:
+            html = createMap(loc)
             # html = createMap(loc_arr)
-            print(html)
+            # print(html)
             # print(msg.value.decode('utf-8'))
             socketio.emit('locations',  html)
-        except Exception as e:
+    except Exception as e:
             print(e)
             print("An exception occurred")
+
+def run_function_every_30_seconds():
+    threading.Timer(10.0, run_function_every_30_seconds).start()
+    main_thread = threading.Thread(target= callMap)
+    main_thread.start()
+
+run_function_every_30_seconds()
    
 
 
@@ -59,4 +75,6 @@ def test_disconnect():
 if __name__ =="__main__":
     socketio.start_background_task(start_consumer)
     socketio.run(app, port=8001)
+    # time.sleep(10)
+    # run_function_every_30_seconds()
     

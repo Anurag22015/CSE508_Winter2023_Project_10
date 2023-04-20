6c78981453b0f4c229213947402db016375ccd78
bsumit43-iiitd <sumit22302@iiitd.ac.in>
2023-04-21 00:14:09 +0530

    Added Preprocess for prediction tweets

--- Final Deliverables/IR_Project_BE/predict.py
+++ Final Deliverables/IR_Project_BE/predict.py
@@ -3,10 +3,16 @@ import torch
 import numpy as np
 import locationtagger
 import nltk
+import emoji
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import spacy
  
 # essential entity models downloads
 nltk.downloader.download('maxent_ne_chunker')
+nltk.downloader.download('stopwords')
 nltk.downloader.download('words')
 nltk.downloader.download('treebank')
 nltk.downloader.download('maxent_treebank_pos_tagger')
@@ -25,6 +31,56 @@ tokenizer = BertTokenizer.from_pretrained(
     do_lower_case = True
     )
 
+
+def dropnull(df):
+  df=df.dropna()
+  return df
+
+def replaceemoji(df):
+    i=0
+    for j in df['text']:
+        df.at[i,'text'] = emoji.demojize(j)
+        i+=1
+    return df
+
+
+def removeurl(df):
+    for i in range(0,len(df)):
+        df.at[i,'text']=re.sub(r'http\S+', '',df.at[i,'text'], flags=re.MULTILINE)
+    return df
+
+def to_lower_case(df):
+    for i in range(0,len(df)):
+        df.at[i,'text']= df.at[i,'text'].lower()
+    return df
+
+def remove_punctuation(df):
+    for i in range(0,len(df)):
+        df.at[i,'text']= df.at[i,'text'].translate(str.maketrans('', '', string.punctuation))
+    return df
+
+def remove_stopwords(df):
+    for i in range(0,len(df)):
+        word=df.iloc[i]['text']
+        word_tokens=word_tokenize(word)
+        stop_words = set(stopwords.words('english'))
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        filtered_sentence = []
+        for w in word_tokens:
+            if w not in stop_words:
+                filtered_sentence.append(w)
+        for j in filtered_sentence:
+            df.at[i,'text']+=" "+str(j)
+    return df
+
+
+def preprocess(df):
+    df = dropnull(df)
+    df = replaceemoji(df)
+    df = to_lower_case(df)
+    df = remove_punctuation(df)
+    df = remove_stopwords(df)
+    return df
 
 
 def preprocessing(input_text, tokenizer):
@@ -62,8 +118,9 @@ def predict(df):
     test_ids = []
     test_attention_mask = []
     if(df.empty):
-        return
+        return []
     else:
+        df = preprocess(df)
         for sentence in df['text']:
             encoding = preprocessing(sentence, tokenizer)
             test_ids.append(encoding['input_ids'])
